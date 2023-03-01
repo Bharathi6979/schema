cb8d3d9e6a4da18ed903e30f882218af0d4ed4f8
Bharathi7969 <bharathiramalingam33@gmail.com>
2023-03-01 13:35:31 +0530

    task completed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { HomePage } from './future'
+import { Header } from './components'
+function App() {
+  return (
+    <>
+      <Header />
+      <HomePage />
+    </>
+
+  );
+}
+
+export default App;
---
+++ src/future/home/data.js
@@ -0,0 +1,45 @@
+export const segmentExistData = [
+    {
+        "segment_name": "segment Arun",
+        "schema": [
+            { "first_name": "First name" },
+            { "last_name": "Last name" }
+        ]
+    },
+    {
+        "segment_name": "segment Vijay",
+        "schema": [
+            { "first_name": "First name" },
+            { "last_name": "Last name" }
+        ]
+    },
+    {
+        "segment_name": "segment Kamal",
+        "schema": [
+            { "first_name": "First name" },
+            { "last_name": "Last name" }
+        ]
+    },
+    {
+        "segment_name": "segment Rajani",
+        "schema": [
+            { "first_name": "First name" },
+            { "last_name": "Last name" }
+        ]
+    },
+    {
+        "segment_name": "segment Surya",
+        "schema": [
+            { "first_name": "First name" },
+            { "last_name": "Last name" }
+        ]
+    },
+    {
+        "segment_name": "segment Vikaram",
+        "schema": [
+            { "first_name": "First name" },
+            { "last_name": "Last name" }
+        ]
+    }
+
+]
